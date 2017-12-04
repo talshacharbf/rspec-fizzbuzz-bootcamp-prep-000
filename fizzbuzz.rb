de45c529aee9f197241462536d6e548f2b27458f
tal shachar <tal.shachar@buzzfeed.com>
2017-12-04 06:39:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,13 @@
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif
+    int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-  else
+  elsif
     int % 5 == 0
     "Buzz"
+  else
+    "nil"
 end
 end
